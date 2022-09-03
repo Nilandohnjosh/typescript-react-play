b8b4f2edc8868f8293decef6b3ca1dd4f964794e
Nilandohnjosh <joshngaoluangraj@gmail.com>
2022-09-03 16:53:46 +1200

    created two components, can render input text thru console

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import NewTodo from './components/NewTodo'
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }]
+
   return (
     <div className="App">
-      <h1>mina</h1>
+      <h1>To Do List</h1>
+      {/* A component that adds todos */}
+      <NewTodo />
+      <TodoList items={todos} />
     </div>
   )
 }
